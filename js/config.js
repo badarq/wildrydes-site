8f9b0bff34fffbe77cffaf8709473f350bd24217
USER NAME <EMAIL ADDRESS>
2020-04-27 23:55:51 -0700

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_JcO2J3hvS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3v2ufdt7nf18do0pak3erv7e9j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
